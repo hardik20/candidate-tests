0cefd23e23346dd1f6955524f67c0b15331edbe5
Hardik <36600312+hardik20@users.noreply.github.com>
2019-01-21 21:57:44 +0100

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 /*
@@ -14,3 +14,14 @@ import App from './App';
 * Please add any other test that you think could be missing to adequately test the App
 *
 * */
+configure({ adapter: new Adapter() });
+
+it('should display an input', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('input').exists()).toEqual(true);
+});
+
+it('should display a button', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('button').exists()).toEqual(true);
+});
